113eecfa4efe8a5a87f3662e760b1e42eafd6f8d
Chris Bird <chris@christopherbird.co.uk>
2010-12-17 00:23:25 +0000

    fixed console sample

--- lib/console.rb
+++ lib/console.rb
@@ -3,39 +3,48 @@ require 'queue'
 require 'item'
 require 'calendar'
 
-@analyst_queue = ItemQueue.new
-@developer_queue = ItemQueue.new
-@qa_queue = ItemQueue.new
-@done_queue = ItemQueue.new
+analyst_queue = ItemQueue.new
+developer_queue = ItemQueue.new
+qa_queue = ItemQueue.new
+done_queue = ItemQueue.new
 		
-@customer = TeamMember.new @analyst_queue, 5
-@analyst = TeamMember.new @developer_queue, 1, @analyst_queue
-@developer = TeamMember.new @qa_queue, 5, @developer_queue
-@qa = TeamMember.new @done_queue, 2, @qa_queue
+for i in 1..10 do
+	item = Item.new 1
+	item.add_estimate_for_role :analyst, 1
+	item.add_estimate_for_role :developer, 2
+	item.add_estimate_for_role :qa, 2
+	analyst_queue.enq item
+end
+
+analyst = TeamMember.new developer_queue, analyst_queue
+analyst.add_role :analyst
+developer = TeamMember.new qa_queue, developer_queue
+developer.add_role :developer
+qa = TeamMember.new done_queue, qa_queue
+qa.add_role :qa
 		
-@calendar = Calendar.new
-@qa.listen_to @calendar
-@developer.listen_to @calendar
-@analyst.listen_to @calendar
-@customer.listen_to @calendar
+calendar = Calendar.new
+qa.listen_to calendar
+developer.listen_to calendar
+analyst.listen_to calendar
 
 puts "Work in progress..."
 puts "analyst   developer qa        done"
 for i in 1..20 do
-	@calendar.move_to_next_day
-  print @analyst_queue.length.to_s.ljust(10)
-  print @developer_queue.length.to_s.ljust(10)
-  print @qa_queue.length.to_s.ljust(10)
-  puts @done_queue.length.to_s
+	calendar.move_to_next_day
+  print analyst_queue.length.to_s.ljust(10)
+  print developer_queue.length.to_s.ljust(10)
+  print qa_queue.length.to_s.ljust(10)
+  puts done_queue.length.to_s
 end
 
 puts ""
 puts "Lead times..."
 puts "started   completed lead time"
-numberOfCompletedItems = @done_queue.length
+numberOfCompletedItems = done_queue.length
 averageLeadTime = 0
 for i in 1..numberOfCompletedItems do
-	item = @done_queue.deq
+	item = done_queue.deq
 	averageLeadTime = averageLeadTime + item.leadTime
 	print item.started_at.to_s.ljust(10)
 	print item.completed_at.to_s.ljust(10)
